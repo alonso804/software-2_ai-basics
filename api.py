56337495fe1228abc59fc33a413cf4e8c6064bfd
Herless Alvarado <herlessalvarado@gmail.com>
2023-11-20 22:05:51 -0500

    added cors

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import chromadb
 from chromadb.config import Settings
 
 app = Flask(__name__)
+CORS(app)
 
 client = chromadb.Client(
     Settings(chroma_db_impl="duckdb+parquet", persist_directory="db/"))
